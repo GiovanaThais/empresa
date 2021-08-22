f8d44b2f88f546ab3c3e656b6cad55bf8e2d2ab9
Andrews Da Mota <andrewscontato1@gmail.com>
2021-08-22 17:20:24 -0300

    feat: add Urls

--- recrutamento/urls.py
+++ recrutamento/urls.py
@@ -13,10 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from api_empresa.views import EmpresaAPIView, ListarEmpresasAPIView, ListarVagasAPIView, VagaAPIView
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from api_empresa import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('vagas', ListarVagasAPIView, VagaAPIView)
+router.register('empresas', ListarEmpresasAPIView, EmpresaAPIView)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('auth/', include('rest_framework.urls')),
+    path('vagas/', ListarVagasAPIView.as_view(), name='listar_vagas'),
+    path('vagas/<int:pk>/', VagaAPIView.as_view(), name='listar_vagas'),
+    path('empresas/', ListarEmpresasAPIView.as_view(), name='listar_empresas'),
+    path('empresas/<int:pk>/', EmpresaAPIView.as_view(), name='listar_empresas'),
 ]
