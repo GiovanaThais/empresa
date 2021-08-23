a320c1e7d99570e3b3b06fb5ba9fc91987c7b518
Giovana Thais <giovana.carvalho@ma9.com.br>
2021-08-23 13:51:19 -0300

    fix:arrumando model

--- core/models.py
+++ core/models.py
@@ -7,26 +7,26 @@ TIPO_CONTRATO_CHOICES = (
 )
 
 class Empresa(models.Model):
-    nome_fantasia = models.CharField(null=False, max_length=50)
-    cnpj = models.CharField(null=False, max_length=20)
-    email = models.EmailField(null=False)
+    nome_fantasia = models.CharField('nome_empresa', null=False, max_length=50)
+    cnpj = models.CharField('cnpj', null=False, max_length=14)
+    email = models.EmailField('email', null=False)
     
     def __str__(self):
         return str(self.nome_fantasia)
 
 class Vaga(models.Model):
-    titulo = models.CharField(max_length=30, null=False)
-    descricao = models.TextField(null=False)
-    salario = models.FloatField(null=False)
-    tipo_contrato = models.CharField(choices=TIPO_CONTRATO_CHOICES, null=False, max_length=50)
-    status = models.BooleanField(null=False, default=1)
+    titulo = models.CharField('titulo', max_length=30, null=False)
+    descricao = models.TextField('descricao', null=False)
+    salario = models.FloatField('salario', null=False)
+    tipo_contrato = models.CharField('tipo_contrato', choices=TIPO_CONTRATO_CHOICES, null=False, max_length=50)
+    status = models.BooleanField('status', null=False, default=1)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='empresa_vaga', verbose_name='Empresa')
 
     def __str__(self):
         return str(self.id)
 
 class Requisito(models.Model):
-    descricao = models.TextField(null=False)
+    descricao = models.TextField('descricao', null=False)
     vaga = models.ForeignKey(Vaga,on_delete=models.CASCADE, related_name='requisitos_vaga', verbose_name='Vaga')
     
     def __str__(self):
